c0c06fedd5fb477743afb6aa61bbcc46f5e8643a
SwannSG <swannsg@gmail.com>
2016-08-21 11:57:45 +0200

    modified:   client/main.js  	modified:   imports/ui/layout.jsx

--- client/main.js
+++ client/main.js
@@ -6,13 +6,27 @@ import { East, North, South, West } from '../imports/ui/hand.jsx';
 import { bm } from '../imports/custom/myJsLib.js'
 import { Button } from '../imports/ui/button.jsx'
 import { BidBox } from '../imports/ui/bidBox.jsx'
+import { Layout } from '../imports/ui/layout.jsx'
 
 bm.shuffle();
 
+$('body').css( "margin", "0" );
+$('body').css( "padding", "0" );
+
 Meteor.startup(() => {
+    console.log('Meteor.startup');
+
+    // const onWindowResize = function() {
+    //     console.log('onWindowResize');
+    //     const vw = $(window).width();
+    //     const vh = $(window).height();
+    //     render(<Layout vh={window.innerHeight} vw={window.innerWidth}/>, document.getElementById('app'));
+    // };
+
+
+
+
     var x = ["ks51", "ts48", "8s46", "2s40", "qh37", "7h32", "3h28", "kc12", "jc10", "5c04", "3c02", "6d18", "3d15"]
-    console.log(window.innerHeight);
-    console.log(window.innerWidth);
 
     // render(<North hand={ bm.north } cardheight={200} meta={bm.handSummary(bm.north)} faceup={true} vulnerable={false}/>, document.getElementById('north'));
     // render(<South hand={ bm.south } cardheight={200} meta={bm.handSummary(bm.south)} faceup={true} vulnerable={false}/>, document.getElementById('south'));
@@ -24,6 +38,9 @@ Meteor.startup(() => {
     //        document.getElementById('app'));
     // render(<Button size={45} value={'value'} label={'label'} style={{}} />, document.getElementById('app'));
 
-    render(<BidBox size={140} lastBid={'1h'}/>, document.getElementById('app'));
+    // render(<BidBox size={140} lastBid={'1h'}/>, document.getElementById('app'));
     // render(<Button size={30} />, document.getElementById('appA'));
+
+    render(<Layout vh={window.innerHeight} vw={window.innerWidth}/>, document.getElementById('app'));
+
 });
--- imports/ui/layout.jsx
+++ imports/ui/layout.jsx
@@ -1,43 +1,141 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { render } from 'react-dom'
+import Radium from 'radium'
 
-export { Container };
+export { Layout }
 
-class Container extends Component {
-    // Container component
-    // this.props
-    //      heightPercent:      percent of viewport height
-    //      orStyle:            {style: value, ...}
-    //      viewportHeight:     in px
-    //      viewportWidth:      in px
-    //      widthPercent:       percent of viewport width
+const styles = {}
+styles.base = {
+    margin: '0',
+    paddding: '0',
+}
 
-    constructor() {
-        super();
-        this.container = {
-            style: {
-                backgroundColor: 'yellow',
-                boxSizing: 'border-box',
-                margin: 0,
-                padding: 0
-            }
-        }
+class Layout extends Component {
+    constructor(props) {
+        super(props);
     }
 
-    dynamicStyle(overwriteStyle={}) {
-        var ds = {
-            height: this.props.heightPercent * this.props.viewportHeight + 'px',
-            width:  this.props.widthPercent * this.props.viewportWidth + 'px',
+    dynamicStyles() {
+        var dimensions = {}
+        dimensions.navHeight = 0.15;
+        dimensions.rhsWidth = 0.3;
+        dimensions.lnHeight = 0.2;              // of main height
+        dimensions.lnWidth = 0.2;               // of main width
+
+        var mainHeight = this.props.vh*(1-dimensions.navHeight);
+        var mainWidth = this.props.vw*(1-dimensions.rhsWidth);
+        var lnHeight = mainHeight*dimensions.lnHeight;
+        var lnWidth = dimensions.lnWidth * mainWidth;
+
+        styles.nav = {
+            height: this.props.vh*dimensions.navHeight,
+            backgroundColor: 'blue'
         }
-        console.log(ds)
-        return Object.assign(this.container.style, ds, this.props.orStyle);
+        styles.main = {
+            float: 'left',
+            height: mainHeight,
+            width: mainWidth,
+            backgroundColor: 'yellow'
+        }
+        styles.rhs = {
+            float: 'right',
+            height: mainHeight,
+            width: this.props.vw*(dimensions.rhsWidth),
+            backgroundColor: 'green'
+        }
+        // north row
+        styles.ln = {
+            float: 'left',
+            height: lnHeight,
+            width: lnWidth,
+            backgroundColor: 'red'
+        }
+        styles.cn = {
+            float: 'left',
+            height: lnHeight,
+            width: mainWidth - 2*lnWidth,
+            backgroundColor: 'purple'
+        }
+        styles.rn = {
+            float: 'left',
+            height: lnHeight,
+            width: lnWidth,
+            backgroundColor: 'black'
+        }
+        // end north row
+        // center row
+        styles.lc = {
+            float: 'left',
+            height: mainHeight - 2*lnHeight,
+            width: lnWidth,
+            backgroundColor: 'brown'
+        }
+        styles.cc = {
+            float: 'left',
+            height: mainHeight - 2*lnHeight,
+            width: mainWidth - 2*lnWidth,
+            backgroundColor: 'indigo'
+        }
+        styles.rc = {
+            float: 'left',
+            height: mainHeight - 2*lnHeight,
+            width: lnWidth,
+            backgroundColor: 'lightblue'
+        }
+        // end center row
+        // south row
+        styles.ls = {
+            float: 'left',
+            height: lnHeight,
+            width: lnWidth,
+            backgroundColor: 'red'
+        }
+        styles.cs = {
+            float: 'left',
+            height: lnHeight,
+            width: mainWidth - 2*lnWidth,
+            backgroundColor: 'purple'
+        }
+        styles.rs = {
+            float: 'left',
+            height: lnHeight,
+            width: lnWidth,
+            backgroundColor: 'black'
+        }
+        // end south row
+
     }
+
+
     render() {
-        return <div style={this.dynamicStyle(this.props.orStyle)}></div>
+        return (
+            <div>
+                <div id="nav" style={[styles.base, styles.nav]}></div>
+                <div id="main" style={[styles.base, styles.main]}>
+                    <div>
+                        <div id="ln" style={[styles.base, styles.ln]}></div>
+                        <div id="cn" style={[styles.base, styles.cn]}></div>
+                        <div id="rn" style={[styles.base, styles.rn]}></div>
+                    </div>
+                    <div>
+                        <div id="lc" style={[styles.base, styles.lc]}></div>
+                        <div id="cc" style={[styles.base, styles.cc]}></div>
+                        <div id="rc" style={[styles.base, styles.rc]}></div>
+                    </div>
+                    <div>
+                        <div id="ls" style={[styles.base, styles.ls]}></div>
+                        <div id="cs" style={[styles.base, styles.cs]}></div>
+                        <div id="rs" style={[styles.base, styles.rs]}></div>
+                    </div>
+                </div>
+                <div id="rhs" style={[styles.base, styles.rhs]}></div>
+            </div>
+        );
     }
 }
 
-{/* <div id="north-row">
-    <div id="north-left"></div>
-    <div id="north-hand"></div>
-    <div id="north-right"></div>
-</div> --> */}
+Layout = Radium(Layout);
+Layout.propTypes = {
+    vh: PropTypes.number.isRequired,      // viewport height
+    vw: PropTypes.number.isRequired,      // viewport width
+};
