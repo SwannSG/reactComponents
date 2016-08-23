6700c0873cfc855b18fd0065a63b21593cd53abd
SwannSG <swannsg@gmail.com>
2016-08-23 18:42:40 +0200

    modified:   client/main.js  	new file:   imports/ui/deal.js  	new file:   imports/ui/deal.jsx  	modified:   package.json

--- client/main.js
+++ client/main.js
@@ -1,61 +1,60 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { Session } from 'meteor/session';
 
 import { East, North, South, West } from '../imports/ui/hand.jsx';
 import { bm } from '../imports/custom/myJsLib.js'
 import { Button } from '../imports/ui/button.jsx'
 import { BidBox } from '../imports/ui/bidBox.jsx'
 import { Layout } from '../imports/ui/layout.jsx'
+import { Deal } from '../imports/ui/deal.jsx'
 
-bm.shuffle();
 globalDimensions = {};
 
 Meteor.startup(() => {
     console.log('Meteor.startup');
-    window.addEventListener('resize',
-        _.debounce(function windowResize(){
-            console.log('Window resize event');
-            render(<Layout vh={$(window).height()} vw={$(window).width()}/>, document.getElementById('app'));
-            render(<North cardheight={globalDimensions.cn.height} hand={ bm.north } hpos={'center'} meta={bm.handSummary(bm.north)} faceup={true} vulnerable={false}/>, document.getElementById('cn'));
-            render(<South cardheight={globalDimensions.cn.height} hand={ bm.south } hpos={'center'} meta={bm.handSummary(bm.south)} faceup={true} vulnerable={false}/>, document.getElementById('cs'));
-            render(<East cardheight={globalDimensions.cn.height} hand={ bm.east } hpos={'center'} meta={bm.handSummary(bm.east)} faceup={false} vulnerable={false}/>, document.getElementById('rc'));
-            render(<West cardheight={globalDimensions.cn.height} hand={ bm.west } hpos={'center'} meta={bm.handSummary(bm.west)} faceup={false} vulnerable={false}/>, document.getElementById('lc'));
-            render(<BidBox size={globalDimensions.rhss.height*0.95} parentDim={globalDimensions.rhss} lastBid={'1h'}/>, document.getElementById('rhss'));
-        }, 200)
-    );
+    // window.addEventListener('resize',
+    //     _.debounce(function windowResize(){
+    //         console.log('Window resize event');
+    //         render(<Layout vh={$(window).height()} vw={$(window).width()}/>, document.getElementById('app'));
+    //         render(<North cardheight={globalDimensions.cn.height} hand={ bm.north } hpos={'center'} meta={bm.handSummary(bm.north)} faceup={true} vulnerable={false}/>, document.getElementById('cn'));
+    //         render(<South cardheight={globalDimensions.cn.height} hand={ bm.south } hpos={'center'} meta={bm.handSummary(bm.south)} faceup={true} vulnerable={false}/>, document.getElementById('cs'));
+    //         render(<East cardheight={globalDimensions.cn.height} hand={ bm.east } hpos={'center'} meta={bm.handSummary(bm.east)} faceup={false} vulnerable={false}/>, document.getElementById('rc'));
+    //         render(<West cardheight={globalDimensions.cn.height} hand={ bm.west } hpos={'center'} meta={bm.handSummary(bm.west)} faceup={false} vulnerable={false}/>, document.getElementById('lc'));
+    //         render(<BidBox size={globalDimensions.rhss.height*0.95} parentDim={globalDimensions.rhss} lastBid={'1h'}/>, document.getElementById('rhss'));
+    //     }, 200)
+    // );
+    //
 
 
 
-
-    var x = ["ks51", "ts48", "8s46", "2s40", "qh37", "7h32", "3h28", "kc12", "jc10", "5c04", "3c02", "6d18", "3d15"]
-
-    // render(<North hand={ bm.north } cardheight={200} meta={bm.handSummary(bm.north)} faceup={true} vulnerable={false}/>, document.getElementById('north'));
-    // render(<South hand={ bm.south } cardheight={200} meta={bm.handSummary(bm.south)} faceup={true} vulnerable={false}/>, document.getElementById('south'));
-    // render(<West hand={ bm.west } cardheight={200}  meta={bm.handSummary(bm.west)} faceup={true}  vulnerable={true}/>, document.getElementById('west'));
-    // render(<East hand={ bm.east } cardheight={200}  meta={bm.handSummary(bm.east)} faceup={true}  vulnerable={true}/>, document.getElementById('east'));
-    // render(<BidBox containerHeight={180} lastBid={'1d'}/>, document.getElementById('bid-box'))
-    // render(<Container heightPercent={0.2} widthPercent={0.334}
-    //        viewportHeight={window.innerHeight} viewportWidth={window.innerWidth}/>,
-    //        document.getElementById('app'));
-    // render(<Button size={45} value={'value'} label={'label'} style={{}} />, document.getElementById('app'));
-
-    // render(<BidBox size={140} lastBid={'1h'}/>, document.getElementById('app'));
-    // render(<Button size={30} />, document.getElementById('appA'));
-
-    // console.log($(window).width());
-    // $(document).ready(function () {
-    //     console.log('document is ready');
-    //     console.log($(window).width());
-    // })
-
-    console.log(globalDimensions);
-
+    // var x = ["ks51", "ts48", "8s46", "2s40", "qh37", "7h32", "3h28", "kc12", "jc10", "5c04", "3c02", "6d18", "3d15"]
+    bm.shuffle();
+    Session.set('dealToggle', 'a')
     render(<Layout vh={$(window).height()} vw={$(window).width()}/>, document.getElementById('app'));
-    render(<North cardheight={globalDimensions.cn.height} hand={ bm.north } hpos={'center'} meta={bm.handSummary(bm.north)} faceup={true} vulnerable={false}/>, document.getElementById('cn'));
+    // render(<North cardheight={globalDimensions.cn.height} hand={ bm.north } hpos={'center'} meta={bm.handSummary(bm.north)} faceup={true} vulnerable={false}/>, document.getElementById('cn'));
+    render(<NorthContainer />, document.getElementById('cn'));
     render(<South cardheight={globalDimensions.cn.height} hand={ bm.south } hpos={'center'} meta={bm.handSummary(bm.south)} faceup={true} vulnerable={false}/>, document.getElementById('cs'));
     render(<East cardheight={globalDimensions.cn.height} hand={ bm.east } hpos={'center'} meta={bm.handSummary(bm.east)} faceup={false} vulnerable={false}/>, document.getElementById('rc'));
     render(<West cardheight={globalDimensions.cn.height} hand={ bm.west } hpos={'center'} meta={bm.handSummary(bm.west)} faceup={false} vulnerable={false}/>, document.getElementById('lc'));
-    render(<BidBox size={globalDimensions.rhss.height*0.95} parentDim={globalDimensions.rhss} lastBid={'1h'}/>, document.getElementById('rhss'));
+    render(<Deal hpos={'center'}/>, document.getElementById('cc'));
+    // render(<BidBox size={globalDimensions.rhss.height*0.95} parentDim={globalDimensions.rhss} lastBid={'1h'}/>, document.getElementById('rhss'));
 
 });
+
+import { createContainer } from 'meteor/react-meteor-data';
+var NorthContainer = createContainer(() => {
+    console.log('NorthContainer');
+    Session.get('dealToggle');
+    return {
+        cardheight: globalDimensions.cn.height,
+        hand: bm.north ,
+        hpos: 'center',
+        meta: bm.handSummary(bm.north),
+        faceup: true,
+        vulnerable: false
+    };
+}, North);
+
+// render(<NorthContainer />, document.getElementById('cn'));
---
+++ imports/ui/deal.jsx
@@ -0,0 +1,40 @@
+import React, { Component, PropTypes } from 'react'
+import { render } from 'react-dom'
+import { Button } from './button.jsx'
+import Radium from 'radium'
+import { bm } from '../custom/myJsLib.js'
+import { Session } from 'meteor/session';
+
+export { Deal }
+
+const styles = {}
+styles.base = {
+}
+
+class Deal extends Component {
+    constructor(props) {
+        super(props);
+        this.updateState = this.updateState.bind(this);
+    }
+
+    updateState() {
+        console.log('deal.updateState');
+        Session.set('dealToggle', Session.get('dealToggle')==='a' ? 'b' : 'a');
+        console.log(Session.get('dealToggle'));
+        bm.shuffle();
+    }
+
+    render() {
+        return (
+            <div>
+                <Button label={'Deal'} value={'deal'} updateState={this.updateState} />
+            </div>
+    )
+    }
+}
+
+{/* label: 'Label',
+onclick: function() {},
+size: 50,
+style: {},
+value: 'value', */}
